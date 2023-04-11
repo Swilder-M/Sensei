ba3df93d58222178e86111c03058766f3e32cb43
Swilder-M <poxiaobbs@gmail.com>
2023-04-12 03:18:53 +0800

    fix: update swift version

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,8 +1,8 @@
 import ProjectDescription
 
 let config = Config(
-    compatibleXcodeVersions: [.upToNextMajor("14.3")],
-    swiftVersion: "5.8",
+    compatibleXcodeVersions: [.upToNextMajor("14.2")],
+    swiftVersion: "5.7",
     generationOptions: .options(
         resolveDependenciesWithSystemScm: true,
         disablePackageVersionLocking: true
